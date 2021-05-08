d2eda43c2987060ef0c654b173d59198e3a230ea
Pradeep <pradeep.negi@evontech.com>
2021-05-09 01:29:04 +0530

    Add Team Rest Controller

---
+++ src/main/java/io/javabrains/ipldashboard/controller/TeamController.java
@@ -0,0 +1,29 @@
+package io.javabrains.ipldashboard.controller;
+
+import io.javabrains.ipldashboard.model.Team;
+import io.javabrains.ipldashboard.repository.MatchRepository;
+import io.javabrains.ipldashboard.repository.TeamRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository,MatchRepository matchRepository){
+        this.teamRepository = teamRepository;
+        this.matchRepository= matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        System.out.println(teamName);
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName,4));
+        return team;
+    }
+}
